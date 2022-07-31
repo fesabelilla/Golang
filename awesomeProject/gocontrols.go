6b46ad420e77e4fda4c9010c264bff1e8b5316ef
fesabelilla <fesabelilla@gmail.com>
2022-07-31 23:45:39 +0600

    range Construct

--- awesomeProject/gocontrols.go
+++ awesomeProject/gocontrols.go
@@ -17,12 +17,41 @@ func forCondition() {
 	}
 }
 
+func rangeConstruct() {
+	nums := []int{1, 2, 3, 4, 5, 6}
+	sum := 0
+	for _, value := range nums {
+		sum += value
+	}
+	fmt.Println("Sum : ", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("Index : ", i)
+		}
+	}
+
+	kvs := map[string]string{"1": "mango", "2": "apple", "3": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s \n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("Key : ", k)
+	}
+
+	for i, c := range "hello" {
+		fmt.Println(i, c)
+	}
+}
+
 func main() {
-	var number int
-	fmt.Printf("Enter a number : ")
-	fmt.Scanln(&number)
-	forLoop(number)
+	//var number int
+	//fmt.Printf("Enter a number : ")
+	//fmt.Scanln(&number)
+	//forLoop(number)
+	//
+	//forCondition()
 
-	forCondition()
-
+	rangeConstruct()
 }
