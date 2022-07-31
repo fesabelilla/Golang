4968dcef9444940767f7717b34a4df01ddaa9226
fesabelilla <fesabelilla@gmail.com>
2022-08-01 00:29:59 +0600

    Function with Single Return

--- awesomeProject/Functions.go
+++ awesomeProject/Functions.go
@@ -13,10 +13,18 @@ func (employee Employee) fullName() {
 	fmt.Println(employee.firstName + " " + employee.lastName)
 }
 
+func sumOfTwoNumber(a int, b int) int {
+	return a + b;
+}
+
 func main() {
 	emp := Employee{
 		firstName: "Zahid",
 		lastName:  " Hasan",
 	}
 	emp.fullName()
+
+	var sum int
+	sum = sumOfTwoNumber(5, 6)
+	fmt.Println(sum)
 }
