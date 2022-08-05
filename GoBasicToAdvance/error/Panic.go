7f205ec5492559df508460f07f358a8dab0c57ea
fesabelilla <fesabelilla@gmail.com>
2022-08-06 00:22:40 +0600

    Go Panic init

---
+++ GoBasicToAdvance/error/Panic.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+	Go panic is a mechanism by which we handle error situations.
+	Panic can be used to abort a function execution.
+	When a function calls panic, its execution stops and the control flows to the associated deferred function.
+	The caller of this function also gets terminated and caller's deferred function gets executed (if present any).
+	This process continues till the program terminates.
+	Now the error condition is reported.
+	This termination sequences is called panicking and can be controlled by the built-in function recover.
+*/
+
+func main() {
+
+}
